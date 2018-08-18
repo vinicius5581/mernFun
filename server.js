45a48b12dda8af51bb2047ffb91646ab3f2c793a
Vinicius <santanaviny@gmail.com>
2018-08-18 16:32:25 -0700

    Adding models and routes for api

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
+const items = require("./routes/api/items");
+
 const app = express();
 
 // BodyParser Middleware
@@ -16,6 +18,9 @@ mongoose
   )
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
+
+// Use routes
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
